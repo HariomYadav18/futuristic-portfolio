20709506cf021f66d57063d8e1a97c3e201baa76
HariomYadav18 <hariomchelsea@gmail.com>
2025-07-28 00:45:58 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,13 +12,13 @@ export default defineConfig({
     minify: true,
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: path.resolve(__dirname, 'index.html')
       }
     }
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   },
   css: {

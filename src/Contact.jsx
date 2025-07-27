0a630bcf2c8b00545c78bcab84761bd3bb8281ba
HariomYadav18 <hariomchelsea@gmail.com>
2025-07-27 19:59:38 +0530

    modified:   src/App.jsx 	new file:   src/Contact.jsx 	new file:   src/Footer.jsx

---
+++ src/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      {/* Contact content will go here */}
+    </section>
+  );
+};
+
+export default Contact; 
